ce19c333a558e0ebf7c4ee00d83de0d23f718ac3
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-24 17:14:17 -0400

    Add style with styled components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Tetris from './components/Tetris';
 
 const App = () => (
-  <div className = "App">
+  <div className="App">
     <Tetris />
   </div>
-)
+);
 
 export default App;
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,22 +1,7 @@
-// A multi dimensional array that
-// represents the grid
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
 
-export const Stage_WIDTH = 12;
-export const Stage_HEIGHT = 20;
-
-
-// 0 means nothing on the grid (clean cell = nothing in that cell)
-// each tetromino will have a letter
-// so we know that the grid will have that
-// particular tetromino in that particular cell.
-// Clear means that there are no tetromino
-// that has collided in this cell and that
-// we should clear it out in the next render. 
-// Clear is going to be set to merge when we 
-// have a tetromino merge into the stage and
-// not be cleared in the next render
-
-export const createStage = () => 
-    Array.from(new Array(Stage_HEIGHT), () =>
-        new Array(Stage_WIDTH).fill([0,'clear'])
-    )
+export const createStage = () =>
+  Array.from(Array(STAGE_HEIGHT), () =>
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
+  )
